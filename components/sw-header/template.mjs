dc25cf85f96fb51db6078dd0742c27dbfadf9ef2
Thon Ly <thonly@heartbank.ceo>
2022-11-08 11:27:54 -0800

    refactor stealth

--- components/sw-header/template.mjs
+++ components/sw-header/template.mjs
@@ -5,111 +5,103 @@ template.innerHTML = `
     <header>
         <section>
             <a href="/"><figure>🎬</figure></a>
-            <a href="/"><h1>THonly™</h1></a>
+            <a href="/"><h1>THonly™ Studios</h1></a>
         </section>
         <input id="menu-toggle" type="checkbox">
         <label class='menu-button-container' for="menu-toggle">
             <span class='menu-button'></span>
         </label>
-        <nav>
-            <ul>
-                <li>
-                    <h3>Music Studio</h3>
-                    <div>
-                        <a href="#Music.Thonly.com"><button>Music.Thonly.com</button></a>
-                        <!--<h4>Inclusion & Diversity</h4>
-                        <menu>
-                            <li><a>Women & Minorities</a></li>
-                            <li><a>Ukrainian Refugees</a></li>
-                            <li><a>Cambodian Genocide</a></li>
-                        </menu>
-                        <h4>Campuses</h4>
-                        <menu>
-                            <li><a>Cambodia</a></li>
-                            <li><a>Ukraine</a></li>
-                            <li><a>United States</a></li>
-                        </menu>-->
-                        <br>
-                        <a href="#Music.HeartBank.Studio"><button>Music.HeartBank.Studio</button></a>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>Animation Studio</h3>
-                    <div>
-                        <a href="#Animation.Thonly.com"><button>Animation.Thonly.com</button></a>
-                        <!--<h4>Explorer</h4>
-                        <menu>
-                            <li><a>Watch Videos on Udemy</a></li>
-                            <li><a>Read Textbook on Medium</a></li>
-                            <li><a href="#ios-quiz">Take Quizzes to Check Understanding</a></li>
-                        </menu>
-                        <h4>Supra Blockchain</h4>
-                        <menu>
-                            <li><a href="#ios-podcast">Podcast: Motivation & Coding Music</a></li>
-                            <li><a href="#ios-code">Interactive Coding Exercises</a></li>
-                            <li><a>Remote Pair Programming on Discord</a></li>
-                            <li><a>Students' Solo Projects on GitHub</a></li>
-                        </menu>
-                        <h4>Infra Blockchains</h4>
-                        <menu>
-                            <li><a>Live Demo: Music Apps</a></li>
-                            <li><a>Syntax Flashcards</a></li>
-                            <li><a>Students' Writings on Medium</a></li>
-                            <li><a>Mock Interviews on YouTube</a></li>
-                        </menu>-->
-                        <br>
-                        <a href="#Animation.HeartBank.Studio"><button>Animation.HeartBank.Studio</button></a>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>Metaverse Studio</h3>
-                    <div>
-                        <a href="#Metaverse.Thonly.com"><button>Metaverse.Thonly.com</button></a>
-                        <!--<h4>Browser</h4>
-                        <menu>
-                            <li><a>Women & Minorities</a></li>
-                            <li><a>Ukrainian Refugees</a></li>
-                            <li><a>Cambodian Genocide</a></li>
-                        </menu>
-                        <h4>DNS</h4>
-                        <menu>
-                            <li><a>Cambodia</a></li>
-                            <li><a>Ukraine</a></li>
-                            <li><a>United States</a></li>
-                        </menu>-->
-                        <br>
-                        <a href="#Metaverse.HeartBank.Studio"><button>Metaverse.HeartBank.Studio</button></a>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>Film Studio</h3>
-                    <div>
-                        <a href="#Film.Thonly.com"><button>Film.Thonly.com</button></a>
-                        <!--<h4>Wallet</h4>
-                        <menu>
-                            <li><a>Watch Videos on Udemy</a></li>
-                            <li><a>Read Textbook on Medium</a></li>
-                            <li><a href="#backend-quiz">Take Quizzes to Check Understanding</a></li>
-                        </menu>
-                        <h4>Personal Identification</h4>
-                        <menu>
-                            <li><a href="#backend-podcast">Podcast: Motivation & Coding Music</a></li>
-                            <li><a href="#backend-code">Interactive Coding Exercises</a></li>
-                            <li><a>Remote Pair Programming on Discord</a></li>
-                            <li><a>Students' Solo Projects on Replit</a></li>
-                        </menu>-->
-                        <br>
-                        <a href="#Film.HeartBank.Studio"><button>Film.HeartBank.Studio</button></a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
+        <ul>
+            <li>
+                <h3>Music Studio</h3>
+                <nav>
+                    <a href="#Music.Thonly.com"><button>Music.Thonly.com</button></a>
+                    <!--<h4>Inclusion & naversity</h4>
+                    <menu>
+                        <li><a>Women & Minorities</a></li>
+                        <li><a>Ukrainian Refugees</a></li>
+                        <li><a>Cambodian Genocide</a></li>
+                    </menu>
+                    <h4>Campuses</h4>
+                    <menu>
+                        <li><a>Cambodia</a></li>
+                        <li><a>Ukraine</a></li>
+                        <li><a>United States</a></li>
+                    </menu>-->
+                    <br>
+                    <a href="#Music.HeartBank.Studio"><button>Music.HeartBank.Studio</button></a>
+                </nav>
+            </li>
+            <li>
+                <h3>Animation Studio</h3>
+                <nav>
+                    <a href="#Animation.Thonly.com"><button>Animation.Thonly.com</button></a>
+                    <!--<h4>Explorer</h4>
+                    <menu>
+                        <li><a>Watch Videos on Udemy</a></li>
+                        <li><a>Read Textbook on Medium</a></li>
+                        <li><a href="#ios-quiz">Take Quizzes to Check Understanding</a></li>
+                    </menu>
+                    <h4>Supra Blockchain</h4>
+                    <menu>
+                        <li><a href="#ios-podcast">Podcast: Motivation & Coding Music</a></li>
+                        <li><a href="#ios-code">Interactive Coding Exercises</a></li>
+                        <li><a>Remote Pair Programming on Discord</a></li>
+                        <li><a>Students' Solo Projects on GitHub</a></li>
+                    </menu>
+                    <h4>Infra Blockchains</h4>
+                    <menu>
+                        <li><a>Live Demo: Music Apps</a></li>
+                        <li><a>Syntax Flashcards</a></li>
+                        <li><a>Students' Writings on Medium</a></li>
+                        <li><a>Mock Interviews on YouTube</a></li>
+                    </menu>-->
+                    <br>
+                    <a href="#Animation.HeartBank.Studio"><button>Animation.HeartBank.Studio</button></a>
+                </nav>
+            </li>
+            <li>
+                <h3>Metaverse Studio</h3>
+                <nav>
+                    <a href="#Metaverse.Thonly.com"><button>Metaverse.Thonly.com</button></a>
+                    <!--<h4>Browser</h4>
+                    <menu>
+                        <li><a>Women & Minorities</a></li>
+                        <li><a>Ukrainian Refugees</a></li>
+                        <li><a>Cambodian Genocide</a></li>
+                    </menu>
+                    <h4>DNS</h4>
+                    <menu>
+                        <li><a>Cambodia</a></li>
+                        <li><a>Ukraine</a></li>
+                        <li><a>United States</a></li>
+                    </menu>-->
+                    <br>
+                    <a href="#Metaverse.HeartBank.Studio"><button>Metaverse.HeartBank.Studio</button></a>
+                </nav>
+            </li>
+            <li>
+                <h3>Film Studio</h3>
+                <nav>
+                    <a href="#Film.Thonly.com"><button>Film.Thonly.com</button></a>
+                    <!--<h4>Wallet</h4>
+                    <menu>
+                        <li><a>Watch Videos on Udemy</a></li>
+                        <li><a>Read Textbook on Medium</a></li>
+                        <li><a href="#backend-quiz">Take Quizzes to Check Understanding</a></li>
+                    </menu>
+                    <h4>Personal Identification</h4>
+                    <menu>
+                        <li><a href="#backend-podcast">Podcast: Motivation & Coding Music</a></li>
+                        <li><a href="#backend-code">Interactive Coding Exercises</a></li>
+                        <li><a>Remote Pair Programming on Discord</a></li>
+                        <li><a>Students' Solo Projects on Replit</a></li>
+                    </menu>-->
+                    <br>
+                    <a href="#Film.HeartBank.Studio"><button>Film.HeartBank.Studio</button></a>
+                </nav>
+            </li>
+        </ul>
         <aside>
             <select onchange="this.getRootNode().host.changeLanguage(event)">
                 <option value="en">English</option>
